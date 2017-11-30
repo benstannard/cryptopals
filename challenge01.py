85983d08c3d72a4c0367857da7d72c8d946bac64
Ben Stannard <ben.stannard@gmail.com>
2017-11-30 14:06:25 -0800

    01 update

--- challenge01.py
+++ challenge01.py
@@ -5,13 +5,14 @@ import base64
 x = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 y = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-def convert_to_base64(s):
+def hex_to_base64(s):
     "input a string type hex. output to base64."
-    decoded = binascii.unhexlify(s)
-    return base64.b64encode(decoded).decode('utf-8')
+    return base64.b64encode(binascii.unhexlify(s)).decode('utf-8')
+
 
 def test():
-    assert convert_to_base64(x) == y
+    assert hex_to_base64(x) == y
     print("[+] All test passed")
+
 
 test()
